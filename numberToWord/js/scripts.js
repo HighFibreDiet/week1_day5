83f1bd779498b0bc2458097b355336e63b35b7fa
Peter Tinti & Robin Miller <student@epicodus.com>
2014-02-21 12:17:31 -0800

    numberToWord function working for 1 through 999

--- numberToWord/js/scripts.js
+++ numberToWord/js/scripts.js
@@ -1,6 +1,26 @@
 var numberToWord = function(number) {
-  return number;
+  var numberDictionary = {1:"one", 2:"two", 3:"three",  4:"four", 5:"five",6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17: "seventeen", 18:"eighteen", 19:"nineteen", 20:"twenty", 30:"thirty", 40:"forty", 50:"fifty", 60:"sixty", 70:"seventy", 80:"eighty", 90:"ninety", 100:"hundred"};
+  var output = "";
+  var length = number.toString().length;
+
+  if(number/100 % 1 === 0) {
+    output += numberDictionary[Math.floor(number/100)] + " " + numberDictionary[100];
+  } else if(number/100 > 1 && number % 10 === 0) {
+    output += numberDictionary[Math.floor(number/100)] + " " + numberDictionary[100] + " " + numberDictionary[number % 100];
+  } else if(number/100 > 1) { 
+    output += numberDictionary[Math.floor(number/100)] + " " + numberDictionary[100] + " " + numberDictionary[(number % 100) - (number % 100 % 10)] + " " + numberDictionary[number % 100 % 10];
+  } else if(number<100 && number > 20) {
+    output += numberDictionary[number - number%10] + " " + numberDictionary[number%10];
+  } else if(number > 10 && number < 20) {
+    output += numberDictionary[number];
+  } else {
+    output += numberDictionary[number];
+  }
+  return output;
 };
+
+
+
 $(function() {
   $("form#scrabble").submit(function(event) {
      var wordToProcess = $("#input").val();
--- numberToWord/spec/specs.js
+++ numberToWord/spec/specs.js
@@ -2,4 +2,29 @@ describe('numberToWord', function() {
   it('returns the word for a single digit when a single digit.', function() {
     numberToWord(1).should.equal("one");
   });
+
+  it('returns the correct word for the tens numbers', function() {
+    numberToWord(20).should.equal("twenty");
+  });
+
+  it('returns a combo of a single digit word plus hundred for any round hundred number', function() {
+    numberToWord(400).should.equal("four hundred");
+  });
+
+  it('returns a combo of a tens number and a single digit for numbers between 21 and 99', function() {
+    numberToWord(78).should.equal("seventy eight");
+  });
+
+  it("returns the correct special word for numbers between 10 and 20", function() {
+    numberToWord(15).should.equal("fifteen");
+  });
+
+  it("returns the correct word for numbers ending in 0 between 110 and 990", function() {
+    numberToWord(15).should.equal("fifteen");
+  });
+
+  it("returns the correct words for numbers between 101 and 999", function() {
+    numberToWord(593).should.equal("five hundred ninety three");
+  });
+
 });
